d9a38c382560bb97a7b8e2226154e959cf851b3a
Inna <innazozulia@outlook.com>
2021-01-31 11:40:06 +0200

    version1.0

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(".slider__inner, .news__slider-inner").slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>'
+  });
+  $("select").styler();
+});
